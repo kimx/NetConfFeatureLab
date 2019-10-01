dadebb4c432d022396f89748b05c4397e0956368
kim <kim@bud4.net>
2019-10-01 16:06:50 +0800

    init

---
+++ NetConfFeatureLab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetConfFeatureLab
+{
+    /// <summary>
+    /// Try.Net 線上文件測試
+    /// </summary>
+    class Program
+    {
+
+        static void Main(string region = null,
+        string[] args = null)
+        {
+            switch (region)
+            {
+                case "intro":
+                    Intro();
+                    break;
+                case "collections":
+                    Collections();
+                    break;
+            }
+        }
+        public static void Intro()
+        {
+            #region intro
+            Console.WriteLine("Hello World!");
+            #endregion
+        }
+
+        public static void Collections()
+        {
+            #region collections
+            var names = new List<string> { "Rain", "Sage", "Lee" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            #endregion
+        }
+    }
+}
---
+++ mydoc/Readme.md
@@ -0,0 +1,3 @@
+### Hello World
+- [Introduction Programming with C#](HelloWorld.md)
+
